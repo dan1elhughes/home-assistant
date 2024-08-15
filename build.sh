ff30a9590f439a661908576e051ac40e5d3f4ec2
Dan Hughes <github@danhughes.dev>
2024-08-15 12:31:48 +0100

    Pre install nunjucks

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 rsync -a --delete static/ dist/
-npx nunjucks-cli \*\*/\*.tpl data.json -o dist -p src -e yaml -O nunjucks.json
+
+# npm i -g nunjucks-cli
+nunjucks \*\*/\*.tpl data.json -o dist -p src -e yaml -O nunjucks.json
+
 yamllint dist
 
 rm -r dist/macros
